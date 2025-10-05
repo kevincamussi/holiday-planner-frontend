6299c91303fd55212adf2feaeca0e497f80081a6
kevincamussi <kevincamussi@gmail.com>
2025-10-05 04:13:17 +0100

    fix(frontend): call /holidays/autocomplete to match router prefix

--- src/api/autocomplete.ts
+++ src/api/autocomplete.ts
@@ -2,9 +2,11 @@ import axios from "axios";
 
 const API_URL = "http://127.0.0.1:8000";
 
-export const getAutocomplete = async (field : "employee_name" | "department"): Promise<string[]> => {
-    const res = await axios.get(`${API_URL}/autocomplete?field=${field}`)
-    return res.data;
-}
-
- 
+export const getAutocomplete = async (
+  field: "employee_name" | "department"
+): Promise<string[]> => {
+  const res = await axios.get(
+    `${API_URL}/holidays/autocomplete?field=${field}`
+  );
+  return res.data;
+};
